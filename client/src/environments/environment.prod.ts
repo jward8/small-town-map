ca2bb1cbbc44ed634cb3eeba64162e7529f587f5
jackmichael <jward8@wisc.edu>
2021-04-20 21:32:25 -0500

    one more try...

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server_url: 'https://smalltownmap.us/api',
+  server_url: 'https://small-town-map.herokuapp.com//api',
   coordinate_endpoint: '/coordinates',
   rss_endpoint: '/rssData',
   env_name: 'prod'
